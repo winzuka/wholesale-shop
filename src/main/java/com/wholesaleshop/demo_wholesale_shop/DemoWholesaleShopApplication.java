2dad5f11af50bdeeca72d90cd019aa93c408f338
winshuka <vinshukakannangara@gmail.com>
2025-03-27 11:51:43 +0530

    Initialized the project

---
+++ src/main/java/com/wholesaleshop/demo_wholesale_shop/DemoWholesaleShopApplication.java
@@ -0,0 +1,13 @@
+package com.wholesaleshop.demo_wholesale_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWholesaleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWholesaleShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wholesaleshop/demo_wholesale_shop/DemoWholesaleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wholesaleshop.demo_wholesale_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWholesaleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
