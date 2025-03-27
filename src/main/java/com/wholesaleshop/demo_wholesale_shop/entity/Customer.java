ef3299839baa25045261d22807d3d24d1429e16e
winshuka <vinshukakannangara@gmail.com>
2025-03-27 12:29:23 +0530

    Created the entity package and customer entity

---
+++ src/main/java/com/wholesaleshop/demo_wholesale_shop/entity/Customer.java
@@ -0,0 +1,60 @@
+package com.wholesaleshop.demo_wholesale_shop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    private
+    Integer customer_id;
+    private String customer_name;
+    private String customer_address;
+    private String customer_email;
+    private String customer_phone;
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_address() {
+        return customer_address;
+    }
+
+    public void setCustomer_address(String customer_address) {
+        this.customer_address = customer_address;
+    }
+
+    public String getCustomer_email() {
+        return customer_email;
+    }
+
+    public void setCustomer_email(String customer_email) {
+        this.customer_email = customer_email;
+    }
+
+    public String getCustomer_phone() {
+        return customer_phone;
+    }
+
+    public void setCustomer_phone(String customer_phone) {
+        this.customer_phone = customer_phone;
+    }
+}
