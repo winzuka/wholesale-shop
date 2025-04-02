47fd7a8c870b8d48338e0aeb8536058df2e1ce4e
winshuka <vinshukakannangara@gmail.com>
2025-04-02 22:21:41 +0530

    Implemented the Swagger and tested the customer controller

---
+++ src/main/java/com/wholesaleshop/demo_wholesale_shop/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.wholesaleshop.demo_wholesale_shop.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("wholesale shop API").version("1.0"));
+    }
+}
